d3d98f174b5cce63fdb2cc8b343f01ecd393cf05
Eugene Storchevoy <s.eugene.a@gmail.com>
2016-08-29 20:05:46 +0200

    [master] changed set origin behaviour.

--- libs/git.rb
+++ libs/git.rb
@@ -2,11 +2,13 @@ require 'git'
 
 class MyGit
   @base = nil
+  @logger = nil
   def initialize(owner, dir, logger)
     @base = Git.open(dir, logger: logger)
+    @logger = logger
     @base.config('user.name', 'Bamboo')
     @base.config('user.email', ENV['GITHUB_MAIL'])
-    set_origin(owner, File.basename(dir)) if @base.remote('origin').url =~ /nothing/
+    set_origin(owner, File.basename(dir)) unless @base.remote('origin').url =~ /git@github.com/
   end
 
   def base
@@ -16,6 +18,7 @@ class MyGit
   def set_origin(owner, repo)
     @base.remote('origin').remove
     @base.add_remote('origin', "git@github.com:#{owner}/#{repo}.git")
+    @logger.info("added origin git@github.com:#{owner}/#{repo}.git")
   end
 
   def is_branch_contains_commit(branch, commit)
